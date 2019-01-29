e84a2993ce2969251b7fd9f0d25432dcdb5208db
toune <ytoune@users.noreply.github.com>
2019-01-30 01:27:23 +0900

    fix

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('C:/git/anime-characters/node_modules/@wapps/gatsby-plugin-material-ui/gatsby-browser.js'),
-      options: {"plugins":[],"productionPrefix":"c"},
-    }]
